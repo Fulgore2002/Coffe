75d345e95ff55df9d01003f26686b2091238363b
Fulgore2002 <typryorhitchcock1@gmail.com>
2025-05-01 16:09:48 -0500

    Add project files.

---
+++ Coffe/Cafe.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Coffe.Utility;
+
+namespace Coffe
+{
+    class Cafe
+    {
+        private List<Beverage> beverages = new List<Beverage>
+        {
+            new Beverage("Double Latte", Beverage.BeverageType.Coffee, Beverage.TemperatureState.Hot, 70),
+            new Beverage("Decaf Coffee", Beverage.BeverageType.Coffee, Beverage.TemperatureState.Hot, 75),
+            new Beverage("Iced Cappuccino", Beverage.BeverageType.Coffee, Beverage.TemperatureState.Cold, 5),
+            new Beverage("Ice Water", Beverage.BeverageType.Water, Beverage.TemperatureState.Cold, 2)
+        };
+
+        public void OpenCafe()
+        {
+            foreach (Beverage beverage in beverages)
+            {
+                beverage.TemperatureChanged += Beverage_TemperatureChanged;
+            }
+
+            SimulateTemperatureChanges();
+        }
+
+    
+        private void Beverage_TemperatureChanged(object sender, TemperatureChangedEventArgs e)
+        {
+            Beverage beverage = sender as Beverage;
+            Console.WriteLine($"{beverage.Name}'s temperature changed from {e.OldTemperature}° to {e.NewTemperature}°.");
+        }
+
+        private void SimulateTemperatureChanges()
+        {
+            for (int i = 0; i < 5; i++) 
+            {
+                foreach (Beverage beverage in beverages)
+                {
+                    beverage.AdjustTemperature();  
+                    Thread.Sleep(500);  
+                }
+
+               Print("--------------------------");
+               Print("--------------------------");
+            }
+        }
+    }
+}
---
+++ Coffe/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Cafe (Event Handlers)
+ * Tyler Hitchcock
+ * 3/13/25
+ * credit: help from my brother Trent
+ */
+
+using Coffe;
+using System;
+
+namespace EventDemo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Title = "Coffe challenge by Tyler Hitchcock";
+            Cafe cafe = new Cafe();
+            cafe.OpenCafe();
+        }
+    }
+}
